e35ad4aed71d30cdeaeb76bb79d5956c60801231
Martin <56572703+martingalmarino@users.noreply.github.com>
2025-10-08 20:57:33 -0300

    solcion cookiehub y favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ 
   subsets: ['latin', 'latin-ext'],
@@ -9,9 +10,29 @@ const inter = Inter({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${inter.variable} font-sans`}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      {/* CookieHub */}
+      <Script
+        src="https://cdn.cookiehub.eu/c2/29a4b0cf.js"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="cookiehub-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            document.addEventListener("DOMContentLoaded", function(event) {
+              var cpm = {};
+              window.cookiehub.load(cpm);
+            });
+          `,
+        }}
+      />
+      
+      <div className={`${inter.variable} font-sans`}>
+        <Component {...pageProps} />
+      </div>
+    </>
   )
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -9,10 +8,7 @@ export default function Document() {
         <meta name="google-site-verification" content="rwB6eu8CzPNSuerN0fPLhk23OsHCFLnO1yWeikj4De8" />
         
         {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -25,24 +21,6 @@ export default function Document() {
       <body className="antialiased bg-background">
         <Main />
         <NextScript />
-        
-        {/* CookieHub */}
-        <Script
-          src="https://cdn.cookiehub.eu/c2/29a4b0cf.js"
-          strategy="afterInteractive"
-        />
-        <Script
-          id="cookiehub-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              document.addEventListener("DOMContentLoaded", function(event) {
-                var cpm = {};
-                window.cookiehub.load(cpm);
-              });
-            `,
-          }}
-        />
       </body>
     </Html>
   )
